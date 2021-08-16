d8346d09579754d21f1664d07cfe5eba90ad5026
am4n-raj <am4nraj@gmail.com>
2021-08-16 13:16:17 +0530

    initial commit

---
+++ emoji/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  function likeHandler(){
+    
+  }
+  return (
+    <div className="App">
+      <h1>Emoji Interpreter</h1>
+      <button onClick={likeHandler}>Like Me</button>
+    </div>
+  );
+}
+
+export default App;
