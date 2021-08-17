345c2e608b8b804c22ea326c8f064da228cc9983
am4n-raj <am4nraj@gmail.com>
2021-08-17 16:05:09 +0530

    input interpreting

--- emoji/src/App.js
+++ emoji/src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
+import React, {useState} from 'react';
+
+const emojiDictionary ={
+  "🙂": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤": "love",
+  "😑":"annoyance"
+};
 
 function App() {
-  function likeHandler(){
+  var[meaning,setMean]=useState(null)
+  function emojiInputHandler(e){
+    var userInput= e.target.value;
     
+    var meaning = emojiDictionary[userInput];
+    if(meaning===undefined){
+      meaning="We don't have this in our database yet";
+    }
+    setMean(meaning)
   }
+  
   return (
     <div className="App">
       <h1>Emoji Interpreter</h1>
-      <button onClick={likeHandler}>Like Me</button>
+      <input className="emojiInput" placeholder="Enter Emoji" onChange={emojiInputHandler} />
+      <h1>{meaning}</h1>
     </div>
   );
 }
