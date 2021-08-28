1c47bc251ae952b022593f9c0f1c4f74da14a374
am4n-raj <am4nraj@gmail.com>
2021-08-28 14:48:11 +0530

    bookdb error

--- emoji/src/book.js
+++ emoji/src/book.js
@@ -1,56 +1,57 @@
 import React, { useState } from "react";
 import "./book.css";
 
-const bookdb = {
-  fiction: [
-    {
-      image:
-        "https://cdn.asaha.com/assets/thumbs/95f/95f873565f638bf31c58361f6201d109.jpg",
-      title: "ABCD",
-      rating: "4/5",
-    },
 
-    {
-      image:
-        "https://i.ytimg.com/an_webp/Wn_9rR65MGE/mqdefault_6s.webp?du=3000&sqp=CLvVl4kG&rs=AOn4CLDLMCKEzmW34RAy3DHsc-KIdDd6WQ",
-      title: "XYZABC",
-      rating: "2/5",
-    },
-
-    {
-      image:
-        "https://i.ytimg.com/an_webp/Wn_9rR65MGE/mqdefault_6s.webp?du=3000&sqp=CLvVl4kG&rs=AOn4CLDLMCKEzmW34RAy3DHsc-KIdDd6WQ",
-      title: "XYZABC",
-      rating: "2/5",
-    }
-  ],
-  poetry: [
-    {
-      image:
-        "https://i.ytimg.com/an_webp/Wn_9rR65MGE/mqdefault_6s.webp?du=3000&sqp=CLvVl4kG&rs=AOn4CLDLMCKEzmW34RAy3DHsc-KIdDd6WQ",
-      title: "ABCD",
-      rating: "4/5",
-    },
-
-    {
-      image:
-        "https://i.ytimg.com/an_webp/Wn_9rR65MGE/mqdefault_6s.webp?du=3000&sqp=CLvVl4kG&rs=AOn4CLDLMCKEzmW34RAy3DHsc-KIdDd6WQ",
-      title: "XYZABC",
-      rating: "2/5",
-    },
-
-    {
-      image:
-        "https://i.ytimg.com/an_webp/Wn_9rR65MGE/mqdefault_6s.webp?du=3000&sqp=CLvVl4kG&rs=AOn4CLDLMCKEzmW34RAy3DHsc-KIdDd6WQ",
-      title: "XYZABC",
-      rating: "2/5",
-    }
-  ]
-};
 
 
 function Books() {
   const [gen, setGen] = useState(null);
+  const bookdb = {
+    fiction: [
+      {
+        image:
+          "https://cdn.asaha.com/assets/thumbs/95f/95f873565f638bf31c58361f6201d109.jpg",
+        title: "ABCD",
+        rating: "4/5",
+      },
+  
+      {
+        image:
+          "https://i.ytimg.com/an_webp/Wn_9rR65MGE/mqdefault_6s.webp?du=3000&sqp=CLvVl4kG&rs=AOn4CLDLMCKEzmW34RAy3DHsc-KIdDd6WQ",
+        title: "XYZABC",
+        rating: "2/5",
+      },
+  
+      {
+        image:
+          "https://i.ytimg.com/an_webp/Wn_9rR65MGE/mqdefault_6s.webp?du=3000&sqp=CLvVl4kG&rs=AOn4CLDLMCKEzmW34RAy3DHsc-KIdDd6WQ",
+        title: "XYZABC",
+        rating: "2/5",
+      }
+    ],
+    poetry: [
+      {
+        image:
+          "https://i.ytimg.com/an_webp/Wn_9rR65MGE/mqdefault_6s.webp?du=3000&sqp=CLvVl4kG&rs=AOn4CLDLMCKEzmW34RAy3DHsc-KIdDd6WQ",
+        title: "ABCD",
+        rating: "4/5",
+      },
+  
+      {
+        image:
+          "https://i.ytimg.com/an_webp/Wn_9rR65MGE/mqdefault_6s.webp?du=3000&sqp=CLvVl4kG&rs=AOn4CLDLMCKEzmW34RAy3DHsc-KIdDd6WQ",
+        title: "XYZABC",
+        rating: "2/5",
+      },
+  
+      {
+        image:
+          "https://i.ytimg.com/an_webp/Wn_9rR65MGE/mqdefault_6s.webp?du=3000&sqp=CLvVl4kG&rs=AOn4CLDLMCKEzmW34RAy3DHsc-KIdDd6WQ",
+        title: "XYZABC",
+        rating: "2/5",
+      }
+    ]
+  };
   const bookObj = Object.keys(bookdb);
   function genreClickHandler(genre) {
     setGen(genre);
@@ -67,11 +68,11 @@ function Books() {
         })}
       <ul>
         {
-        bookdb[gen].map(function (book){
-          return(console.log(book.name)) 
-            
-        }
-        )
+        bookdb[gen].map((book)=>(
+          <li key={book.name}>{book.name}</li>
+        ))
+        
+        
         }
         </ul>
       </div>
